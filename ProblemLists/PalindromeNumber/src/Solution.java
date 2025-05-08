dae9449834989e7aee042b3cd085398158be4b62
x1ptadkm <x1ptadkm@gmail.com>
2025-05-08 21:52:32 +0700

    x1pta

---
+++ ProblemLists/PalindromeNumber/src/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Solution{
+    public boolean isPalindrome(int x){
+        if(x<0) return false;
+        
+        long ans=0;
+        int y=x;
+        
+        while(y>0){
+            ans=ans*10+y%10;
+            y/=10;
+        }
+        
+        return ans==x;
+    }
+    
+    public static void main(String[] args){
+        Scanner inp=new Scanner(System.in);
+        int x=inp.nextInt();
+        
+        Solution tester=new Solution();
+        System.out.println(tester.isPalindrome(x));
+    }
+}
