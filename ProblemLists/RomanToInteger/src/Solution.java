6d8d2256152e6b5b3298a5f6c894eaf3ef6d9776
x1ptadkm <x1ptadkm@gmail.com>
2025-05-09 21:58:30 +0700

    x1pta

--- ProblemLists/RomanToInteger/src/Solution.java
+++ ProblemLists/RomanToInteger/src/Solution.java
@@ -6,12 +6,12 @@ public class Solution{
     int[] roman=new int[128];
 
     roman['I']=1;
-    roman['V']= 5;
-    roman['X']= 10;
-    roman['L']= 50;
-    roman['C']= 100;
-    roman['D']= 500;
-    roman['M']= 1000;
+    roman['V']=5;
+    roman['X']=10;
+    roman['L']=50;
+    roman['C']=100;
+    roman['D']=500;
+    roman['M']=1000;
 
     for (int i = 0; i + 1 < s.length(); ++i)
       if (roman[s.charAt(i)] < roman[s.charAt(i + 1)])
