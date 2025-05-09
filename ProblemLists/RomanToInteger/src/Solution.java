dabbcf0df0e0692f4f58ac4aefda0ee899442c3b
x1ptadkm <x1ptadkm@gmail.com>
2025-05-09 22:03:12 +0700

    x1pta

--- ProblemLists/RomanToInteger/src/Solution.java
+++ ProblemLists/RomanToInteger/src/Solution.java
@@ -13,7 +13,7 @@ public class Solution{
     roman['D']=500;
     roman['M']=1000;
 
-    for(int i= 0; i+1 < s.length(); i++)
+    for(int i=0; i+1 < s.length(); i++)
       if(roman[s.charAt(i)] < roman[s.charAt(i+1)])
         ans-=roman[s.charAt(i)];
       else
