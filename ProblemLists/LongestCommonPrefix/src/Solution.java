06f88b94c367196679d03a56630399ede7078739
x1ptadkm <x1ptadkm@gmail.com>
2025-05-09 22:20:03 +0700

    x1pta

--- ProblemLists/LongestCommonPrefix/src/Solution.java
+++ ProblemLists/LongestCommonPrefix/src/Solution.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
-class Solution {
-  public String longestCommonPrefix(String[] strs) {
-    if (strs.length == 0)
+class Solution{
+  public String longestCommonPrefix(String[] strs){
+    if(strs.length==0)
       return "";
 
-    for (int i = 0; i < strs[0].length(); ++i)
-      for (int j = 1; j < strs.length; ++j)
-        if (i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i))
+    for(int i=0; i<strs[0].length(); i++)
+      for(int j=1; j<strs.length; j++)
+        if(i==strs[j].length() || strs[j].charAt(i)!=strs[0].charAt(i))
           return strs[0].substring(0, i);
 
     return strs[0];
   }
 
   public static void main(String[] args) {
-    Scanner inp = new Scanner(System.in);
-    Solution tester = new Solution();
+    Scanner inp=new Scanner(System.in);
+    Solution tester=new Solution();
 
-    System.out.print("Nhập các chuỗi (cách nhau bằng dấu cách): ");
-    String input = inp.nextLine().trim();
+    System.out.print("strs = ");
+    String input=inp.nextLine().trim();
 
-    // Tách chuỗi bằng dấu cách
-    String[] strs = input.split("\\s+");
+    // tách chuỗi bằng dấu cách
+    String[] strs=input.split("\\s+");
 
-    String prefix = tester.longestCommonPrefix(strs);
+    String prefix=tester.longestCommonPrefix(strs);
     System.out.println("Tiền tố chung dài nhất là: \"" + prefix + "\"");
   }
 }
