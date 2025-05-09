2bde3996429f2bff3e56a9cd88f2f1ccda60bf91
x1ptadkm <x1ptadkm@gmail.com>
2025-05-09 22:06:02 +0700

    x1pta

--- ProblemLists/LongestCommonPrefix/src/Solution.java
+++ ProblemLists/LongestCommonPrefix/src/Solution.java
@@ -5,9 +5,9 @@ class Solution{
     if(strs.length==0)
       return "";
 
-    for(int i = 0; i < strs[0].length(); ++i)
-      for(int j = 1; j < strs.length; ++j)
-        if (i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i))
+    for(int i=0; i < strs[0].length(); ++i)
+      for(int j=1; j < strs.length; ++j)
+        if(i==strs[j].length() || strs[j].charAt(i)!=strs[0].charAt(i))
           return strs[0].substring(0, i);
 
     return strs[0];
